fc9c8b7fc3d7560dfa49aaab7239157de2977d57
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-04 11:02:31 +0200

    Testing Jenkinsfile

--- docker-config/database/runDatabase.sh
+++ docker-config/database/runDatabase.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo docker network inspect jee7-network 1>/dev/null 2>/dev/null
+echo "lol"
+docker network inspect jee7-network 1>/dev/null 2>/dev/null
 if [ $? -ne 0 ]; then
-	sudo docker network create --subnet 172.18.0.0/16   jee7-network
+	docker network create --subnet 172.18.0.0/16   jee7-network
 fi
 
 
 if [[ "$(docker images -q jee7-demo-mysql 2> /dev/null)" == "" ]]; then
- 	sudo docker build -t jee7-demo-mysql .
+ 	docker build -t jee7-demo-mysql .
 fi
-sudo docker run --ip="172.18.0.3" --net="jee7-network" -p 13306:3306 -e MYSQL_ROOT_PASSWORD=admin -d jee7-demo-mysql
+docker run --ip="172.18.0.3" --net="jee7-network" -p 13306:3306 -e MYSQL_ROOT_PASSWORD=admin -d jee7-demo-mysql
