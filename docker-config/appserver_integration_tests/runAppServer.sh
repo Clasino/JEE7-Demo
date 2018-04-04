07ecb130cbd0897e404dc2773295d14418539686
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-04 17:07:59 +0200

    Testing Jenkinsfile New

--- docker-config/appserver_integration_tests/runAppServer.sh
+++ docker-config/appserver_integration_tests/runAppServer.sh
@@ -3,4 +3,4 @@
 if [[ "$(docker images -q jee7-test-wildfly 2> /dev/null)" == "" ]]; then
  	docker build -t jee7-test-wildfly .
 fi
-docker run -d  -p 18080:18080 -p 19990:19990 -p 18777:18787  jee7-test-wildfly
+docker run -d  -p 8080:8080 -p 9990:9990 -p 8777:8787  jee7-test-wildfly
