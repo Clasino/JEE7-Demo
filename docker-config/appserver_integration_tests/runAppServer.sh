c697b2611cd1e091417603460f8afa558cbd8267
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-13 10:33:38 +0200

    Debuging Jenkins

--- docker-config/appserver_integration_tests/runAppServer.sh
+++ docker-config/appserver_integration_tests/runAppServer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+CONTAINER_NAME='bodacious_goren'
+
 if [[ "$(docker images -q jee7-test-wildfly 2> /dev/null)" == "" ]]; then
  	docker build -t jee7-test-wildfly .
 fi
-docker run -d  -p 18080:8080 -p 9990:9990 -p 8787:8787  jee7-test-wildfly
+if [[docker inspect -f {{.State.Running}} $CONTAINER_NAME]]; then
+    docker rm -fv $CONTAINER_NAME
+fi
+docker run -d  -p 18080:8080 -p 9990:9990 -p 8787:8787 --name=$CONTAINER_NAME  jee7-test-wildfly
--- docker-config/database/runDatabase.sh
+++ docker-config/database/runDatabase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CONTAINER_NAME='cheeky_binko'
+
 docker network inspect jee7-network 1>/dev/null 2>/dev/null
 if [ $? -ne 0 ]; then
 	docker network create --subnet 172.18.0.0/16   jee7-network
@@ -9,4 +11,7 @@ fi
 if [[ "$(docker images -q jee7-demo-mysql 2> /dev/null)" == "" ]]; then
  	docker build -t jee7-demo-mysql .
 fi
-docker run --ip="172.18.0.3" --net="jee7-network" -p 13306:3306 -e MYSQL_ROOT_PASSWORD=admin -d jee7-demo-mysql
+if [[docker inspect -f {{.State.Running}} $CONTAINER_NAME]]; then
+    docker rm -fv $CONTAINER_NAME
+fi
+docker run --ip="172.18.0.3" --net="jee7-network" -p 13306:3306 --name=$CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=admin -d jee7-demo-mysql
