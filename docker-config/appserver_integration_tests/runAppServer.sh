ce9ab228dcd695958d9e95a87c1da76b4966ab61
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-13 10:57:46 +0200

    Debuging Jenkins

--- docker-config/appserver_integration_tests/runAppServer.sh
+++ docker-config/appserver_integration_tests/runAppServer.sh
@@ -8,4 +8,4 @@ fi
 if docker inspect -f {{.State.Running}} $CONTAINER_NAME; then
     docker rm -fv $CONTAINER_NAME
 fi
-docker run -d  -p 18080:8080 -p 9990:9990 -p 8787:8787 --name=$CONTAINER_NAME  jee7-test-wildfly
+docker run -d  -p 8080:8080 -p 9990:9990 -p 8787:8787 --name=$CONTAINER_NAME  jee7-test-wildfly
